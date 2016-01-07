85179a6395d182a865c5857e35b91296d68b4272
flavorhythm <zeyuki@gmail.com>
2016-01-07 11:50:19 -0600

    Changed CustomDialogFragment's design

--- app/src/main/java/com/zenoyuki/flavorhythm/gaslogger/MainActivity.java
+++ app/src/main/java/com/zenoyuki/flavorhythm/gaslogger/MainActivity.java
@@ -1,6 +1,5 @@
 package com.zenoyuki.flavorhythm.gaslogger;
 
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,7 +7,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
