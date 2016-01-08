6d8211f155e1fd9a0f9ef4ee0d5bc2854eacbf7d
flavorhythm <zeyuki@gmail.com>
2016-01-08 16:57:51 -0600

    Created menu item for HistoryActivity

--- app/src/main/java/com/zenoyuki/flavorhythm/gaslogger/HistoryActivity.java
+++ app/src/main/java/com/zenoyuki/flavorhythm/gaslogger/HistoryActivity.java
@@ -1,7 +1,10 @@
 package com.zenoyuki.flavorhythm.gaslogger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import java.util.ArrayList;
 
@@ -22,6 +25,31 @@ public class HistoryActivity extends AppCompatActivity {
         listView.setEmptyView(findViewById(R.id.empty_txt_emptyListText));
 
         refreshData();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_history, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.MI_del_all:
+                //createDialog
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
