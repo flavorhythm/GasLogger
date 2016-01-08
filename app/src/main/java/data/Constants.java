5976153db3f341cc0e49556c9d97732a4525a211
flavorhythm <zeyuki@gmail.com>
2016-01-08 16:47:09 -0600

    Changed topup gallons datatype from float to double

--- app/src/main/java/data/Constants.java
+++ app/src/main/java/data/Constants.java
@@ -4,7 +4,7 @@ package data;
  * Created by zyuki on 11/30/2015.
  * Constants class holding SQL database/table/column names
  */
-public class Constants {
+public final class Constants {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "fuel_db";
     public static final String TABLE_NAME = "fueling_tbl";
--- app/src/main/java/model/FuelLog.java
+++ app/src/main/java/model/FuelLog.java
@@ -8,21 +8,21 @@ public class FuelLog {
     //Values that are stored into DB
     private int itemID;                 //Unique ID for every entry. Saved into DB column "_id"
     private int currentOdomVal;       //
-    private float fuelTopupAmount;      //
+    private double fuelTopupAmount;      //
     private long recordDate;          //
     private boolean skipEntry;
 
     //Setters
     public void setItemID(int itemID) {this.itemID = itemID;}
     public void setCurrentOdomVal(int currentTachVal) {this.currentOdomVal = currentTachVal;}
-    public void setFuelTopupAmount(float fuelTopup) {this.fuelTopupAmount = fuelTopup;}
+    public void setFuelTopupAmount(double fuelTopup) {this.fuelTopupAmount = fuelTopup;}
     public void setRecordDate(long recordDate) {this.recordDate = recordDate;}
     public void setSkipEntry(boolean skipEntry) {this.skipEntry = skipEntry;}
 
     //Getters
     public int getItemID() {return itemID;}
     public int getCurrentOdomVal() {return currentOdomVal;}
-    public float getFuelTopupAmount() {return fuelTopupAmount;}
+    public double getFuelTopupAmount() {return fuelTopupAmount;}
     public long getRecordDate() {return recordDate;}
     public boolean getSkipEntry() {return skipEntry;}
 }
