7776f5d3aaad9b85e840dc9ad8b61603f7e24327
flavorhythm <zeyuki@gmail.com>
2016-01-08 16:46:21 -0600

    Made Constants class a static import
    all Constants.VALUE changed to just VALUE

--- app/src/main/java/data/DatabaseHandler.java
+++ app/src/main/java/data/DatabaseHandler.java
@@ -6,11 +6,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 import model.FuelLog;
+
+import static data.Constants.*;
 
 /**
  * Created by zyuki on 11/30/2015.
@@ -23,18 +23,18 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     //Custom constructor which takes in @PARAM context, the only parameter that may differ. Other parameters are static and are set in the super call.
     public DatabaseHandler(Context context) {
-        super(context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     //onCreate override. Creates SQL database for the first time using the column names below
     @Override
     public void onCreate(SQLiteDatabase db) {
         //Creates a SQL command string
-        String CREATE_FUEL_LOG_TABLE = "CREATE TABLE " + Constants.TABLE_NAME + "(" +
-                Constants.KEY_ID + " INTEGER PRIMARY KEY, " +
-                Constants.ODOM_VAL + " INTEGER, " +
-                Constants.FUEL_AMOUNT + " REAL, " +
-                Constants.RECORD_DATE + " INTEGER);";
+        String CREATE_FUEL_LOG_TABLE = "CREATE TABLE " + TABLE_NAME + "(" +
+                KEY_ID + " INTEGER PRIMARY KEY, " +
+                ODOM_VAL + " INTEGER, " +
+                FUEL_AMOUNT + " REAL, " +
+                RECORD_DATE + " INTEGER);";
 
         //Passes SQL command
         db.execSQL(CREATE_FUEL_LOG_TABLE);
@@ -44,7 +44,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //Passes SQL command to remove old database
-        db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         //Creates new database
         onCreate(db);
     }
@@ -60,18 +60,18 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         //Cursor object to cycle through database entries
         //query params are as follows: table name, string of column names, ..., sort by
         Cursor cursor = db.query(
-                Constants.TABLE_NAME,
+                TABLE_NAME,
                 new String[] {
-                        Constants.KEY_ID,
-                        Constants.ODOM_VAL,
-                        Constants.FUEL_AMOUNT,
-                        Constants.RECORD_DATE
+                        KEY_ID,
+                        ODOM_VAL,
+                        FUEL_AMOUNT,
+                        RECORD_DATE
                 },
                 null,
                 null,
                 null,
                 null,
-                Constants.RECORD_DATE + " DESC"
+                RECORD_DATE + " DESC"
         );
 
         //If and Do statement that cycles through all entries in the database.
@@ -79,10 +79,10 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             do {
                 FuelLog fuelLog = new FuelLog(); //Creates new fuelLog object
 
-                fuelLog.setItemID(cursor.getInt(cursor.getColumnIndex(Constants.KEY_ID))); //Sets fuelLog object's itemID variable to data within database
-                fuelLog.setCurrentOdomVal(cursor.getInt(cursor.getColumnIndex(Constants.ODOM_VAL))); //Sets fuelLog object's currentOdomVal variable to data within database
-                fuelLog.setFuelTopupAmount(cursor.getFloat(cursor.getColumnIndex(Constants.FUEL_AMOUNT))); //Sets fuelLog object's fuelTopupAmount variable to data within database
-                fuelLog.setRecordDate(cursor.getLong(cursor.getColumnIndex(Constants.RECORD_DATE)));
+                fuelLog.setItemID(cursor.getInt(cursor.getColumnIndex(KEY_ID))); //Sets fuelLog object's itemID variable to data within database
+                fuelLog.setCurrentOdomVal(cursor.getInt(cursor.getColumnIndex(ODOM_VAL))); //Sets fuelLog object's currentOdomVal variable to data within database
+                fuelLog.setFuelTopupAmount(cursor.getDouble(cursor.getColumnIndex(FUEL_AMOUNT))); //Sets fuelLog object's fuelTopupAmount variable to data within database
+                fuelLog.setRecordDate(cursor.getLong(cursor.getColumnIndex(RECORD_DATE)));
 
                 fuelLogList.add(fuelLog); //Adds the entire fuelLog object into the fuelLogList
             } while(cursor.moveToNext());
@@ -99,11 +99,11 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(Constants.ODOM_VAL, entry.getCurrentOdomVal());
-        values.put(Constants.FUEL_AMOUNT, entry.getFuelTopupAmount());
-        values.put(Constants.RECORD_DATE, System.currentTimeMillis());
+        values.put(ODOM_VAL, entry.getCurrentOdomVal());
+        values.put(FUEL_AMOUNT, entry.getFuelTopupAmount());
+        values.put(RECORD_DATE, System.currentTimeMillis());
 
-        db.insert(Constants.TABLE_NAME, null, values);
+        db.insert(TABLE_NAME, null, values);
         db.close();
     }
 
@@ -111,8 +111,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
 
         db.delete(
-                Constants.TABLE_NAME,
-                Constants.KEY_ID + " = ?",
+                TABLE_NAME,
+                KEY_ID + " = ?",
                 new String[]{String.valueOf(id)}
         );
 
@@ -123,14 +123,14 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(Constants.ODOM_VAL, entry.getCurrentOdomVal());
-        values.put(Constants.FUEL_AMOUNT, entry.getFuelTopupAmount());
-        values.put(Constants.RECORD_DATE, System.currentTimeMillis());
+        values.put(ODOM_VAL, entry.getCurrentOdomVal());
+        values.put(FUEL_AMOUNT, entry.getFuelTopupAmount());
+        values.put(RECORD_DATE, System.currentTimeMillis());
 
         db.update(
-                Constants.TABLE_NAME,
+                TABLE_NAME,
                 values,
-                Constants.KEY_ID + " = ?",
+                KEY_ID + " = ?",
                 new String[]{String.valueOf(entry.getItemID())}
         );
 
